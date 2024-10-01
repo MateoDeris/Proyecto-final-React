1938ae369c330ce754465d182aaca800135b5979
Mateo Deris <164948383+MateoDeris@users.noreply.github.com>
2024-09-30 23:15:22 -0300

    error...2

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -18,3 +18,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app); // Exporta la instancia de Firestore
+g
